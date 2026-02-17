5cd5e9426070b43793db5c13badd291cf261e9ab
Leena Rashid <your_github_email@example.com>
2026-02-16 20:47:41 -0700

    Images of Day-16

--- 2026/day-16/Scripts/hello.sh
+++
@@ -1,7 +0,0 @@
-<< comment
-This is going to be my fist script
-It can work without SHEBANG,as bash shell is the default shell,
-can be executed with bash or ./,either ways
-comment
-
-echo "HELLO DevOps!!!"
